0d63c11a412c1614fab1cc6904165dc190b7cb9a
Safaa Magdy <engsafaamagdy1024@gmail.com>
2026-02-15 00:29:24 +0200

    authontication

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getAllMessages, getUnreadMessages, markAsRead, sendMessage } from "../controllers/messageController.js";
+import { authAdmin } from "../auth.js";
 
 
 const messageRouter = express.Router();
 
 messageRouter.post("/", sendMessage);
-messageRouter.get("/", getAllMessages);
-messageRouter.get("/unread", getUnreadMessages);
-messageRouter.put("/:id/read", markAsRead);
+messageRouter.get("/",authAdmin, getAllMessages);
+messageRouter.get("/unread",authAdmin, getUnreadMessages);
+messageRouter.put("/:id/read",authAdmin, markAsRead);
 
 export default messageRouter;
